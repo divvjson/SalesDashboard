4f5a5d915d9edb2a33854067d846fbc74d8d0348
David Johansson <david.n.johansson@live.com>
2024-07-07 11:34:06 +0200

    wip

--- SalesDashboard/Program.cs
+++ SalesDashboard/Program.cs
@@ -1,10 +1,26 @@
+using Microsoft.EntityFrameworkCore;
 using SalesDashboard.Components;
+using SalesDashboard.Entities;
+using SalesDashboard.Helpers;
+using SalesDashboard.Services.Scoped.LocalStorage;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContextFactory<AdventureWorksContext>(options =>
+{
+    var dataSource = builder.Environment.IsDevelopment() ? SecretsHelper.GetValue("DbDataSource") : "localhost";
+    var initialCatalog = SecretsHelper.GetValue("DbInitialCatalog");
+    var userId = SecretsHelper.GetValue("DbUserID");
+    var password = SecretsHelper.GetValue("DbPassword");
+
+    options
+        .UseLazyLoadingProxies()
+        .UseSqlServer($"Data Source={dataSource};Initial Catalog={initialCatalog};User ID={userId};Password={password};Encrypt=False");
+});
+
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+builder.Services.AddScoped<LocalStorageService>();
 
 var app = builder.Build();
 
@@ -17,11 +33,14 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+if (app.Environment.IsDevelopment())
+{
+    app.Urls.Add("https://localhost:4200");
+    app.Urls.Add("https://0.0.0.0:4200");
+}
 
 app.Run();
---
+++ SalesDashboard/Services/Scoped/LocalStorage/LocalStorageService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.JSInterop;
+using System.Text.Json;
+
+namespace SalesDashboard.Services.Scoped.LocalStorage
+{
+    public class LocalStorageService(IJSRuntime jSRuntime)
+    {
+        public async Task<T?> GetItemAsync<T>(string key)
+        {
+            var value = await jSRuntime.InvokeAsync<string?>("localStorage.getItem", key);
+
+            if (value is null)
+            {
+                return default;
+            }
+
+            var valueDeserialized = JsonSerializer.Deserialize<T>(value);
+
+            return valueDeserialized;
+        }
+
+        public async Task SetItemAsync<T>(string key, T value)
+        {
+            var valueSerialized = JsonSerializer.Serialize(value);
+
+            await jSRuntime.InvokeVoidAsync("localStorage.setItem", key, valueSerialized);
+        }
+
+        public async Task RemoveItemAsync(string key)
+        {
+            await jSRuntime.InvokeVoidAsync("localStorage.removeItem", key);
+        }
+
+        public async Task ClearAsync()
+        {
+            await jSRuntime.InvokeVoidAsync("localStorage.clear");
+        }
+    }
+}
