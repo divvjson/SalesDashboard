32a9005bae7efc0614cf3507659ef7e6fc9787e4
davidjohanssonbf <89454414+davidjohanssonbf@users.noreply.github.com>
2024-07-16 12:59:45 +0200

    wip

---
+++ SalesDashboard/Pages/Regional/SalesTrendTop3Countries/Sale.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesDashboard.Pages.Regional.SalesTrendTop3Countries
+{
+    public class Sale
+    {
+        public required int Year { get; set; }
+        public required string CountryName { get; set; }
+        public required decimal TotalSales { get; set; }
+    }
+}
