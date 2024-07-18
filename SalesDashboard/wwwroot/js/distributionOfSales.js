c81848dd6b1316afaafe718342c9baeb4ab6df56
davidjohanssonbf <89454414+davidjohanssonbf@users.noreply.github.com>
2024-07-18 17:07:11 +0200

    wip

--- SalesDashboard/wwwroot/js/distributionOfSales.js
+++ SalesDashboard/wwwroot/js/distributionOfSales.js
@@ -1,4 +1,5 @@
 ﻿let distributionOfSalesMap;
+let circles = [];
 
 window.initializeMap = () => {
     const distributionOfSalesMapElement = document.getElementById('distributionOfSalesMap');
@@ -17,5 +18,34 @@ window.initializeMap = () => {
 };
 
 window.updateMap = (stateProvinceSales) => {
+    for (const circle of circles) {
+        circle.setMap(null);
+    }
 
+    circles = [];
+
+    for (const stateProvinceSale of stateProvinceSales) {
+        const scaleFactor = 150; // Adjust this factor to control the size of the circles
+        const circle = new google.maps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: distributionOfSalesMap,
+            center: { lat: stateProvinceSale.latitude, lng: stateProvinceSale.longitude },
+            radius: Math.sqrt(stateProvinceSale.totalSales) * scaleFactor
+        });
+
+        const infoWindow = new google.maps.InfoWindow({
+            content: `<div><strong>${stateProvinceSale.countryRegionName}</strong><br>Sales: ${stateProvinceSale.totalSales}</div>`
+        });
+
+        circle.addListener('click', () => {
+            infoWindow.setPosition(circle.getCenter());
+            infoWindow.open(distributionOfSalesMap);
+        });
+
+        circles.push(circle);
+    }
 };
