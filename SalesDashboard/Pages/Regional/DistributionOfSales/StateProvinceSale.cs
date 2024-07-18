fc04273bb8a9ebd938cc84c64989f184e6ac29af
davidjohanssonbf <89454414+davidjohanssonbf@users.noreply.github.com>
2024-07-18 16:28:49 +0200

    wip

---
+++ SalesDashboard/Pages/Regional/DistributionOfSales/StateProvinceSale.cs
@@ -0,0 +1,17 @@
+﻿namespace SalesDashboard.Pages.Regional.DistributionOfSales
+{
+    public class StateProvinceSale
+    {
+        public required int StateProvinceId { get; set; }
+
+        public required string StateProvinceName { get; set; }
+
+        public required string CountryRegionName { get; set; }
+
+        public required double Latitude { get; set; }
+
+        public required double Longitude { get; set; }
+
+        public required decimal TotalSales { get; set; }
+    }
+}
