f9b496709fb29a73820d0c533d3b30e310e69f63
davidjohanssonbf <89454414+davidjohanssonbf@users.noreply.github.com>
2024-07-18 10:53:14 +0200

    wip

---
+++ SalesDashboard/Components/Shared/SelectCountryDialog/SelectCountryGridItem.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesDashboard.Components.Shared.SelectCountryDialog
+{
+    public class SelectCountryGridItem
+    {
+        public required string Name { get; set; }
+
+        public required string CountryRegionCode { get; set; }
+    }
+}
