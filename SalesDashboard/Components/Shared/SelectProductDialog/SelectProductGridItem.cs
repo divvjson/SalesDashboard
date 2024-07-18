2b137e2cc6e1bde5922b246c75f0a25cdf9e174c
davidjohanssonbf <89454414+davidjohanssonbf@users.noreply.github.com>
2024-07-18 10:18:29 +0200

    wip

---
+++ SalesDashboard/Components/Shared/SelectProductDialog/SelectProductGridItem.cs
@@ -0,0 +1,13 @@
+﻿namespace SalesDashboard.Components.Shared.SelectProductDialog
+{
+    public class SelectProductGridItem
+    {
+        public required string Name { get; set; }
+
+        public required string ProductNumber { get; set; }
+
+        public required string? ProductSubcategoryName { get; set; }
+
+        public required string? ProductCategoryName { get; set; }
+    }
+}
