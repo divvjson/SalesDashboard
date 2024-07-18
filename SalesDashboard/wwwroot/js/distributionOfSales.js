0cc499e69097d74db75696c465e71ab94a3cc575
davidjohanssonbf <89454414+davidjohanssonbf@users.noreply.github.com>
2024-07-18 12:41:22 +0200

    wip

---
+++ SalesDashboard/wwwroot/js/distributionOfSales.js
@@ -0,0 +1,19 @@
+﻿let map;
+
+window.initializeMap = () => {
+    if (!map) {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: 0, lng: 0 },
+            disableDoubleClickZoom: true,
+            fullscreenControl: false,
+            mapTypeControl: false,
+            streetViewControl: false,
+            zoomControl: false,
+            zoom: 2
+        });
+    }
+};
+
+window.updateMap = (salesData) => {
+
+};
