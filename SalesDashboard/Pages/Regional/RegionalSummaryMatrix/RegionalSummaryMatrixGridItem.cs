a889475e09d80ad21150cc25e867b64f4b275b68
davidjohanssonbf <89454414+davidjohanssonbf@users.noreply.github.com>
2024-07-24 15:45:19 +0200

    wip

---
+++ SalesDashboard/Pages/Regional/RegionalSummaryMatrix/RegionalSummaryMatrixGridItem.cs
@@ -0,0 +1,19 @@
+﻿namespace SalesDashboard.Pages.Regional.RegionalSummaryMatrix
+{
+    public class RegionalSummaryMatrixGridItem
+    {
+        public required string CountryRegionName { get; set; }
+
+        public required string StateProvinceName { get; set; }
+
+        public required string City { get; set; }
+
+        public required decimal Sales { get; set; }
+
+        public required int QuantitySold { get; set; }
+
+        public required decimal GrossProfit { get; set; }
+
+        public required decimal GrossMargin { get; set; }
+    }
+}
