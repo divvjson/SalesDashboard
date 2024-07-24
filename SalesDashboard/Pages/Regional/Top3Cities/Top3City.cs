8b768ac45a980a64565b8f48c1cab05cb74980bf
davidjohanssonbf <89454414+davidjohanssonbf@users.noreply.github.com>
2024-07-24 11:14:13 +0200

    wip

---
+++ SalesDashboard/Pages/Regional/Top3Cities/Top3City.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesDashboard.Pages.Regional.Top3Cities
+{
+    public class Top3City
+    {
+        public required string City { get; set; }
+
+        public required decimal TotalSales { get; set; }
+    }
+}
