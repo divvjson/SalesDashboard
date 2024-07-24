2735be4cf0c2a491d5170ec739d70fca849b3c71
davidjohanssonbf <89454414+davidjohanssonbf@users.noreply.github.com>
2024-07-24 17:57:42 +0200

    wip

--- SalesDashboard/Extensions/DecimalExtensions.cs
+++ SalesDashboard/Extensions/DecimalExtensions.cs
@@ -7,6 +7,11 @@
             return $"${value / 1_000_000:0.00}M";
         }
 
+        public static string ToThousandWithTwoDecimals(this decimal value)
+        {
+            return $"${value / 1_000:0.00}K";
+        }
+
         public static string ToPercentageWithTwoDecimals(this decimal value)
         {
             return value.ToString("0.00" + "%");
