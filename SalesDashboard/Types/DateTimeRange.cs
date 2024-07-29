330f2da4344189dfedfbf3b74c905742f748a0d6
David Johansson <david.n.johansson@live.com>
2024-07-29 16:28:06 +0200

    wip

---
+++ SalesDashboard/Types/DateTimeRange.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesDashboard.Types
+{
+    public class DateTimeRange
+    {
+        public required DateTime? Start { get; set; }
+
+        public required DateTime? End { get; set; }
+    }
+}
