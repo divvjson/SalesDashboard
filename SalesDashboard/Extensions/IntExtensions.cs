8670081981fdd6c9abfaaf1c293a174c6c208a8d
David Johansson <david.n.johansson@live.com>
2024-07-30 10:32:27 +0200

    wip

---
+++ SalesDashboard/Extensions/IntExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace SalesDashboard.Extensions
+{
+    public static class IntExtensions
+    {
+        public static string ToTwoDecimalsInThousands(this int value)
+        {
+            var scaled = value / 1000.0;
+
+            var formattedScaled = scaled.ToString("F2") + "K";
+
+            return formattedScaled;
+        }
+    }
+}
