e2d9bd7df2e4c9944cf1c169d5d2d52f0b27462b
David Johansson <david.n.johansson@live.com>
2024-07-30 16:36:21 +0200

    wip

---
+++ SalesDashboard/Pages/Product/TotalSalesByCategory/TotalSalesByCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesDashboard.Pages.Product.TotalSalesByCategory
+{
+    public class TotalSalesByCategory
+    {
+        public required string ProductCategoryName { get; set; }
+
+        public required double Percentage { get; set; }
+    }
+}
