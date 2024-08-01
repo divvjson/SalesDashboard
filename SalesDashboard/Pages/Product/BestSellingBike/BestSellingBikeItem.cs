eb5e210f34c2ada618de60a290f4849d9094cf72
David Johansson <david.n.johansson@live.com>
2024-08-01 10:50:14 +0200

    wip

---
+++ SalesDashboard/Pages/Product/BestSellingBike/BestSellingBikeItem.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesDashboard.Pages.Product.BestSellingBike
+{
+    public class BestSellingBikeItem
+    {
+        public required string BikeName { get; set; }
+
+        public required int QuantitySold { get; set; }
+    }
+}
