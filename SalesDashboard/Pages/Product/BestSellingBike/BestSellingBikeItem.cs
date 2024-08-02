250647503093991a007ee29bcd83bbbf79c8feef
David Johansson <david.n.johansson@live.com>
2024-08-02 11:52:10 +0200

    wip

--- SalesDashboard/Pages/Product/BestSellingBike/BestSellingBikeItem.cs
+++ SalesDashboard/Pages/Product/BestSellingBike/BestSellingBikeItem.cs
@@ -2,6 +2,8 @@
 {
     public class BestSellingBikeItem
     {
+        public required int ProductId { get; set; }
+
         public required string BikeName { get; set; }
 
         public required int QuantitySold { get; set; }
