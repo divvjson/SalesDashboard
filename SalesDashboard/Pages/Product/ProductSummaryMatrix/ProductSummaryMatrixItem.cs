44bf7da28304d6b3d3a25b9a004f9be747ff1b18
David Johansson <david.n.johansson@live.com>
2024-08-04 12:38:41 +0200

    wip

---
+++ SalesDashboard/Pages/Product/ProductSummaryMatrix/ProductSummaryMatrixItem.cs
@@ -0,0 +1,21 @@
+﻿namespace SalesDashboard.Pages.Product.ProductSummaryMatrix
+{
+    public class ProductSummaryMatrixItem
+    {
+        public required string ProductCategoryName { get; set; }
+
+        public required string ProductSubcategoryName { get; set; }
+
+        public required string ProductName { get; set; }
+
+        public required int QuantitySold { get; set; }
+
+        public required decimal TotalSales { get; set; }
+
+        public required decimal TotalCosts { get; set; }
+
+        public required decimal GrossProfit { get; set; }
+
+        public required decimal GrossMargin { get; set; }
+    }
+}
