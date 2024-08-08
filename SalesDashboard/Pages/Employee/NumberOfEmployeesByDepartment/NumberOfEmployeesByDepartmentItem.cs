f1a05401ae2ef8b84a5769a447403d0082dca59c
davidjohanssonbf <89454414+davidjohanssonbf@users.noreply.github.com>
2024-08-08 16:37:05 +0200

    wip

---
+++ SalesDashboard/Pages/Employee/NumberOfEmployeesByDepartment/NumberOfEmployeesByDepartmentItem.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesDashboard.Pages.Employee.NumberOfEmployeesByDepartment
+{
+    public class NumberOfEmployeesByDepartmentItem
+    {
+        public required string DepartmentName { get; set; }
+
+        public required int EmployeeCount { get; set; }
+    }
+}
