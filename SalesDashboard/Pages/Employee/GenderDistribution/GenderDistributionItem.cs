26fec80a34ff19bef3ec3c51bab05676a1ba2c7a
davidjohanssonbf <89454414+davidjohanssonbf@users.noreply.github.com>
2024-08-09 14:07:59 +0200

    wip

---
+++ SalesDashboard/Pages/Employee/GenderDistribution/GenderDistributionItem.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesDashboard.Pages.Employee.GenderDistribution
+{
+    public class GenderDistributionItem
+    {
+        public required string Gender { get; set; }
+
+        public required double Percentage { get; set; }
+    }
+}
