298ad99bf776a5e117c5de4554c35b677e2dd57c
davidjohanssonbf <89454414+davidjohanssonbf@users.noreply.github.com>
2024-08-14 15:09:36 +0200

    wip

---
+++ SalesDashboard/Pages/Employee/EmployeeLocation/EmployeeLocationComponent.razor.js
@@ -0,0 +1,17 @@
+﻿let employeeLocationMap;
+
+export function initializeMap() {
+    const employeeLocationMapElement = document.getElementById('employeeLocationMap');
+
+    if (employeeLocationMapElement) {
+        employeeLocationMap = new google.maps.Map(employeeLocationMapElement, {
+            center: { lat: 15, lng: 5 },
+            disableDoubleClickZoom: true,
+            fullscreenControl: false,
+            mapTypeControl: false,
+            streetViewControl: false,
+            zoomControl: false,
+            zoom: 2
+        });
+    }
+}
