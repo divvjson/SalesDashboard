21a0e8394b54b93c4efa48120a04f7b583fea13a
davidjohanssonbf <89454414+davidjohanssonbf@users.noreply.github.com>
2024-08-14 16:36:50 +0200

    wip

---
+++ SalesDashboard/Pages/Employee/EmployeeLocation/EmployeeLocationItem.cs
@@ -0,0 +1,27 @@
+﻿namespace SalesDashboard.Pages.Employee.EmployeeLocation
+{
+    public class EmployeeLocationItem
+    {
+        public required int BusinessEntityId { get; set; }
+
+        public required string FirstName { get; set; }
+
+        public required string LastName { get; set; }
+
+        public required string JobTitle { get; set; }
+
+        public required string DepartmentName { get; set; }
+
+        public required string? Address { get; set; }
+
+        public required string? City { get; set; }
+
+        public required string? StateProvinceName { get; set; }
+
+        public required string? CountryRegionName { get; set; }
+
+        public required double? Latitude { get; set; }
+
+        public required double? Longitude { get; set; }
+    }
+}
