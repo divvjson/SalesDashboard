527226670a358ccdd61987f9c7583a7812b617b1
David Johansson <david.n.johansson@live.com>
2024-08-17 10:33:23 +0200

    wip

--- SalesDashboard/Pages/Employee/EmployeeLocation/EmployeeLocationComponent.razor.js
+++ SalesDashboard/Pages/Employee/EmployeeLocation/EmployeeLocationComponent.razor.js
@@ -1,4 +1,5 @@
 ﻿let employeeLocationMap;
+let markers = [];
 
 export function initializeMap() {
     const employeeLocationMapElement = document.getElementById('employeeLocationMap');
@@ -14,4 +15,37 @@ export function initializeMap() {
             zoom: 2
         });
     }
+}
+
+export function updateMap(employeeLocationItems) {
+    clearMarkers();
+
+    const infoWindow = new google.maps.InfoWindow();
+
+    for (const employeeLocationItem of employeeLocationItems) {
+        if (employeeLocationItem.latitude && employeeLocationItem.longitude) {
+            const marker = new google.maps.Marker({
+                position: {
+                    lat: employeeLocationItem.latitude,
+                    lng: employeeLocationItem.longitude
+                },
+                map: employeeLocationMap,
+                title: `${employeeLocationItem.firstName} ${employeeLocationItem.lastName}`
+            });
+
+            markers.push(marker);
+
+            marker.addListener('click', () => {
+                infoWindow.setContent(`<div><strong>${employeeLocationItem.firstName} ${employeeLocationItem.lastName}</strong></div>`);
+                infoWindow.open(employeeLocationMap, marker);
+            });
+        }
+    }
+}
+
+function clearMarkers() {
+    for (let i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+    markers = [];
 }
