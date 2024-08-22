9210b393fc1ebbf01325f8edf6bf6e934b505849
davidjohanssonbf <89454414+davidjohanssonbf@users.noreply.github.com>
2024-08-22 10:18:57 +0200

    wip

---
+++ SalesDashboard/Pages/Employee/MaritalStatus/MaritalStatusItem.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesDashboard.Pages.Employee.MaritalStatus
+{
+    public class MaritalStatusItem
+    {
+        public required string MaritalStatus { get; set; }
+
+        public required double Percentage { get; set; }
+    }
+}
