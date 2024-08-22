acfb80bc6b36ca33cff0c76a1354482b8d4d8c12
davidjohanssonbf <89454414+davidjohanssonbf@users.noreply.github.com>
2024-08-22 13:45:31 +0200

    wip

---
+++ SalesDashboard/Extensions/DoubleExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace SalesDashboard.Extensions
+{
+    public static class DoubleExtensions
+    {
+        public static string ToCurrencyWithTwoDecimals(this double value)
+        {
+            var formattedValue = value.ToString("C2", new CultureInfo("en-US"));
+            
+            return formattedValue;
+        }
+    }
+}
---
+++ SalesDashboard/Pages/Employee/Top5BestSellingSalesPersonsYTD/Top5BestSellingSalesPersonsYTDItem.cs
@@ -0,0 +1,11 @@
+﻿namespace SalesDashboard.Pages.Employee.Top5BestSellingSalesPersonsYTD
+{
+    public class Top5BestSellingSalesPersonsYTDItem
+    {
+        public required string FirstName { get; set; }
+
+        public required string LastName { get; set; }
+
+        public required double SalesYtd { get; set; }
+    }
+}
