9b2bcb7816c220abffa53086747cd89afb98345f
davidjohanssonbf <89454414+davidjohanssonbf@users.noreply.github.com>
2024-08-28 15:03:28 +0200

    wip

---
+++ SalesDashboard/Components/StateProviders/BreakpointStateProvider/BreakpointStateProviderComponent.razor.js
@@ -0,0 +1,9 @@
+﻿export function initializeResizeListener(dotNetObj) {
+    window.addEventListener('resize', () => {
+        dotNetObj.invokeMethodAsync('HandleResize');
+    });
+}
+
+export function getScreenWidth() {
+    return window.innerWidth;
+}
