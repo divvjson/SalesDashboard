dee34ca404ad16ec4e32405fccaec60d70809d6e
David Johansson <david.n.johansson@live.com>
2024-09-15 13:49:03 +0200

    wip

--- SalesDashboard/Services/AdventureWorksDbCommandInterceptor.cs
+++ SalesDashboard/Services/AdventureWorksDbCommandInterceptor.cs
@@ -22,11 +22,12 @@ namespace SalesDashboard.Services
         public override ValueTask<InterceptionResult<DbDataReader>> ReaderExecutingAsync(DbCommand command, CommandEventData eventData, InterceptionResult<DbDataReader> result, CancellationToken cancellationToken = default)
         {
             var circuitId = GetCircuitId(command.CommandText);
-            // sanitized command text = <-----
 
             if (!string.IsNullOrEmpty(circuitId))
             {
-                _service.LatestDbCommandTextSubject.OnNext(new(circuitId, command.CommandText));
+                var sanitized = SanitizeTagFromCommandText(command.CommandText);
+
+                _service.LatestDbCommandTextSubject.OnNext(new(circuitId, sanitized));
             }
 
             return new ValueTask<InterceptionResult<DbDataReader>>(result);
@@ -48,6 +49,32 @@ namespace SalesDashboard.Services
             var circuitId = commandText[startIndex..endIndex];
 
             return circuitId;
+        }
+
+        public static string SanitizeTagFromCommandText(string commandText)
+        {
+            // Loop to remove all tags following the pattern
+            while (true)
+            {
+                // Find the start of any tag
+                int startIndex = commandText.IndexOf("-- ");
+                int endIndex = commandText.IndexOf("_TAG_END");
+
+                // If no more tags are found, break the loop
+                if (startIndex == -1 || endIndex == -1)
+                    break;
+
+                // Adjust the endIndex to include the "_TAG_END"
+                endIndex += "_TAG_END".Length;
+
+                // Remove the part of the string containing the tag
+                commandText = commandText.Remove(startIndex, endIndex - startIndex).Trim();
+            }
+
+            // After removing all tags, also remove any new line characters
+            commandText = commandText.Replace("\r\n", " ").Replace("\n", " ").Trim();
+
+            return commandText;
         }
     }
 }
