78bd7f29891cca9a393109fa6ca4a0b218470c4c
davidjohanssonbf <89454414+davidjohanssonbf@users.noreply.github.com>
2024-09-20 11:13:55 +0200

    wip

--- SalesDashboard/Services/CircuitService.cs
+++ SalesDashboard/Services/CircuitService.cs
@@ -2,13 +2,20 @@
 
 namespace SalesDashboard.Services
 {
-    public class CircuitService(CircuitAccessor circuitAccessor) : CircuitHandler
+    public class CircuitService(CircuitAccessor circuitAccessor, AdventureWorksDbCommandService dbCommandService) : CircuitHandler
     {
         public override async Task OnCircuitOpenedAsync(Circuit circuit, CancellationToken cancellationToken)
         {
             circuitAccessor.CircuitId = circuit.Id;
 
             await base.OnCircuitOpenedAsync(circuit, cancellationToken);
+        }
+
+        public override async Task OnCircuitClosedAsync(Circuit circuit, CancellationToken cancellationToken)
+        {
+            dbCommandService.RemoveDbCommandInfoByCircuitId(circuit.Id);
+
+            await base.OnCircuitClosedAsync(circuit, cancellationToken);
         }
     }
 }
